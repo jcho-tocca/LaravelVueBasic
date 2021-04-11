5a862d1e215b41a58918a23235b5a4ad5a8c0b4c
jcho-tocca <jaeman.cho@tocca-net.jp>
2021-04-11 16:13:02 +0900

    vueの基本アーキテクチャ

---
+++ resources/views/top.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+        <title>{{ env('APP_NAME') }}</title>
+    </head>
+    <body>
+        @if (Route::has('login'))
+            <div>
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <div id="app">
+            <app></app>
+        </div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
