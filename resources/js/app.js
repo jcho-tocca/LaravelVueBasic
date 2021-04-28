7d2b34e317dd7d6c5295f7dcc8ecfd08cf519f9b
jcho-tocca <jeaman0321@gmail.com>
2021-04-28 12:27:57 +0900

    npm install vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,17 @@ require('./bootstrap');
 
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-
+import Vuetify from 'vuetify';
 import router from './Router'
 import store from './Store';
 import App from './App.vue'
 
+Vue.use(Vuetify);
 Vue.use(VueRouter)
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
     router,
     store,
     components: { App }
